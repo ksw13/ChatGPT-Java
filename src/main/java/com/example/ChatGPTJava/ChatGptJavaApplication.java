3663104dde156c60970740d80509fc485f55737e
ksw13 <ksw48483@gmail.com>
2023-05-24 10:13:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/ChatGPTJava/ChatGptJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChatGPTJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChatGPTJava/ChatGptJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatGPTJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
